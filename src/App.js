bebd5db65af30e2cfc9da1f207ba35570566a656
mohamed azizi <azizi.med01@gmail.com>
2021-03-26 00:25:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useEffect,useState } from "react";
+import { useDispatch } from "react-redux";
+import { Switch, Route } from "react-router-dom";
+import Adminlogin from "./components/Adminlogin";
+import Loginsup from "./components/Loginsup";
+import Profilesup from "./components/Profilesup";
+import Profileadmin from "./components/Profileadmin";
+import { supadminLogin, adminLogin } from "./JS/actions";
+import { BrowserRouter } from "react-router-dom";
+import Contact from "./Client/Pages/Contact";
+import Partners from "./Client/Pages/Partners";
+import FAQ from "./Client/Pages/FAQ";
+import Agents from "./Client/Pages/Agents";
+import Offers from "./Client/Pages/Offers";
+import Footer from "./components/Footer";
+import Nav from "./components/NavBar/Nav";
+import Home from "./Client/Pages/Home";
+import About from "./Client/Pages/About";
+import Root from "./Root";
+function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const superlog = window.localStorage.getItem("superAdminSesame");
+    const adminlog = window.localStorage.getItem("adminSesame");
+    dispatch(supadminLogin(JSON.parse(superlog)));
+    dispatch(adminLogin(JSON.parse(adminlog)));
+  }, []);
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/admin">
+          <Adminlogin />
+        </Route>
+        <Route exact path="/superadmin">
+          <Loginsup />
+        </Route>
+        <Route exact path="/superadmin/profile">
+          <Profilesup />
+        </Route>
+        <Route exact path="/admin/profile">
+          <Profileadmin />
+        </Route>
+        
+        <Route exact path="/">          
+           <Nav />
+           <Home/>
+        </Route>
+        <Route exact path="/about">          
+           <Nav />
+           <About/>
+        </Route>
+        <Route exact path="/offers">
+          <Nav />
+          <Offers/>
+        </Route>
+       
+        <Route exact path="/agents">
+           <Agents/>
+        </Route>
+        <Route exact path="/partners">
+           <Partners/>
+        </Route>
+      
+              
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
